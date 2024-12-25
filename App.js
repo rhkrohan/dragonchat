ea1e7d9c9d3c7a6176c95ffa265000089109b1b2
Rohan <rohan17343@gmail.com>
2024-12-25 18:55:49 -0500

    Added the design funcitonality to the header

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
-import ComponentsScreen from "./src/screens/ComponentsScreen"
+import { StyleSheet, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
 
 const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Components: ComponentsScreen
-  },
-  {
-    initialRouteName: "Components",
-    defaultNavigationOptions: {
-      title: "App",
-    },
-  }
+	{
+		Home: HomeScreen,
+		Components: ComponentsScreen,
+	},
+	{
+		initialRouteName: 'Home',
+		defaultNavigationOptions: {
+			title: 'Dragon Chat',
+			headerBackground: () => {
+				return <View style={styles.headerBackground}></View>;
+			},
+		},
+	},
 );
+
+const styles = StyleSheet.create({
+	headerBackground: {
+		flex: 1,
+		backgroundColor: 'red',
+	},
+});
 
 export default createAppContainer(navigator);
